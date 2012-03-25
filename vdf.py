4636aeecb673a61b3211ae07ae95711f9c446710
JT <dieselmachine@gmail.com>
2012-03-25 02:17:58 -0700

    vdf.py initial commit

---
+++ vdf.py
@@ -0,0 +1,33 @@
+from cStringIO import StringIO
+
+def get_next(s):
+    c = s.read(1)
+    if c == '"':
+        " string "
+        data = ''
+        while True:
+            c = s.read(1)
+            if c == '"':
+                break
+            data += c
+    elif c == '{':
+        " dict "
+        data = {}
+        while True:
+            key = get_next(s)
+            if key:
+                value = get_next(s)
+                data[key] = value
+            else:
+                return data
+            next = s.read(1)
+            if next == '}':
+                break
+            s.seek(-1,1)
+    elif c == '}':
+        return None
+    return data
+    
+def parse_vdf(data):
+    data = StringIO('{%s}' % data.replace('\t','').replace('\n',''))
+    return get_next(data)
